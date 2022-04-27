e3e2d7187387486420a8ee9d7a2386e1cbfd83c2
samgujral <71089547+samgujral@users.noreply.github.com>
2022-04-27 18:11:58 +0100

    fifth commit

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let count = 0;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  res.send(`This is count number ${count}.`);
+
 })
 
+app.get('/increment', (req, res) => {
+
+  count += 1;
+  res.send(`Your count has been incremented and is now ${count}.`)
+
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
