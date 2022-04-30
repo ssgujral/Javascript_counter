4e7dde7f15833f77770815768a2f8d3e10c02a22
samgujral <71089547+samgujral@users.noreply.github.com>
2022-04-30 15:43:01 +0100

    Database connection working, increment page working

--- index.js
+++ index.js
@@ -2,29 +2,52 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const { Client } = require('pg');
-const client = new Client();
-await client.connect();
+const { Pool, Client } = require('pg');
 
-//const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-//console.log(res.rows[0].message) // Hello world!
-//await client.end()
+const connectionString = 'postgresql://worldwide@localhost:5432/web_counter'
 
-let count = 0;
+app.get('/', (req, res) => 
+{
+  
+  const client = new Client({
+  
+      connectionString,
+  
+  });
+  
+  client.connect();
+  
+  
+  client.query('SELECT count FROM count WHERE id = 1;')
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e.stack));
 
-app.get('/', (req, res) => {
-
-  res.send(`This is count number ${count}.`);
+      
 
 });
 
 app.get('/increment', (req, res) => {
 
-  count += 1;
-  res.send(`Your count has been incremented and is now ${count}.`);
+  const client = new Client({
+  
+    connectionString,
 
 });
 
+client.connect();
+
+client.query('UPDATE count SET count = count + 1 WHERE id = 1;')
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e.stack));
+
+
+
+
+
+  //count += 1;
+  //res.send(`Your count has been incremented and is now ${count}.`);
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
